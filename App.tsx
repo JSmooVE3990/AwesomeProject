348550f88a62da761d33fc4be9e5e42f1027cab4
JSmooVE3990 <Jurrand.Summerville.08@gmail.com>
2024-01-25 21:19:25 -0500

    Stack nav practice

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Game from './src/Game';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Profile from './src/Profile';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer >
-      <Game/>
+      <Stack.Navigator>
+        <Stack.Screen name ="Home" component={Game} options={{title:'Welcome'}}/>
+        <Stack.Screen name="Profile" component={Profile} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
